c66263921574a54e0e02719bebbf6fb3d8b6ab28
Vaibhav Khanna <vaibhav.home88@gmail.com>
2025-01-13 20:39:15 +0530

    Landing Page Hero Section

--- frontend/pages/Home/Home.jsx
+++ frontend/pages/Home/Home.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 import styles from './Home.module.css'
 import Navbar from '../../components/Navbar/Navbar'
 
+
 const Home = () => {
   return (
-    <div style={{backgroundColor:'cornsilk',height:'70vh'}} className={styles.homeContainer}>
-        Home
+    <div style={{minHeight:'100vh'}} className={styles.homeContainer}>
+        <div className={styles.heroBox}>
+          <div className={styles.heroContent}>
+            <h1 className={styles.heroTitle}>Discover Your Perfect <span className={styles.heroTitleSpan}>Plant for Every</span> Occasion</h1>
+            <p className={styles.heroText}>Welcome to your ultimate plant destination! Explore our diverse collection of plants tailored for gifting, home decor, and indoor living, including low-light varieties that thrive without sunlight.</p>
+            <button className={styles.heroButton}>Shop Now</button>
+          </div>
+        </div>
     </div>
   )
 }
